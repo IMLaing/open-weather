30d8aba658e2f3a9163b65b676865f6a872ec695
IMLaing <ianmlaing@gmail.com>
2016-10-30 10:35:30 -0400

    made some changes to the html

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,10 +1,17 @@
 angular.module('owmApp', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {
-            templateURL: 'home.html',
+            templateUrl: 'home.html',
             controller: "homeCtrl"
+        }).when('/city', {
+            templateUrl: 'city.html',
+            controller: 'cityCtrl'
         });
     }])
     .controller('homeCtrl', function ($scope) {
+
+    })
+    .controller('cityCtrl', function ($scope) {
+        $scope.city = 'New York';
 
     });
---
+++ test.js
@@ -0,0 +1,20 @@
+need to build single page email sign up w / input box - email - newsletter - tested that the submission works -
+    if error styles were included
+    - allowed submit to go through or not - commited to repo
+Syntax is different
+
+Event Delegation -
+    target parent ul - defined
+
+Hoisting -
+    going to search through whole file even
+if it is defiend afterwards.
+
+types of data -
+
+    operator
+
+Javascript is a TYPE BASED Language - it is checking
+for type as well.
+
+Favorite Question - Outside learning workshops I will have available - culture quesitons - Why is the position available.
