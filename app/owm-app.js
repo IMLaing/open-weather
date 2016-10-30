e0c941e8d0294b44da91493df69b946499769009
IMLaing <ianmlaing@gmail.com>
2016-10-30 11:25:02 -0400

    worked out $routeParams working on whitelist value

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,18 +1,24 @@
+'use strict';
+
 angular.module('owmApp', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {
             templateUrl: 'home.html',
             controller: "homeCtrl"
-        }).when('/city', {
+        }).when('/city/:city', { // This states the /:city will be the first returned as $routeParams.city
             templateUrl: 'city.html',
             controller: 'cityCtrl'
         });
     }])
     .controller('homeCtrl', function ($scope) {
-
+        //Empty Controller
     })
-    .controller('cityCtrl', ['$routeParams', function ($scope, $routeParams)] {
-        $scope.city = $routeParams.city;
-
-
-    });
+    .controller('cityCtrl', function ($scope, $routeParams) { //uses the $routeParams service which is built in to AngularJS - ngRoute 
+        var cityCurrent = $routeParams.city; //the city variable in the html gets ther parameter of city for the cityCurrent
+        if (owmcities.indexOf(city) === -1) {
+            console.log('city not found');
+            return;
+        }
+        $scope.city = cityCurrent;
+    })
+    .value('owmCities', ['NewYork', 'Dallas', 'Chicago']);
